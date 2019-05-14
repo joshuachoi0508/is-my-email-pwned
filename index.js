8a42df0835af6dc428e366709a4b228527e6bebd
Josh Choi <joshuachoi0508@gmail.com>
2019-05-14 16:56:39 -0700

    Create sample routes for get, post, put, and delete

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const express = require('express');
 // set up express app
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('GET request');
-  res.send({name: 'Yoshi'});
-  res.end();
-})
-
 //listen for requests
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests!');
---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/ninjas', (req, res) => {
+  res.send({type: "GET"})
+});
+
+router.post('/ninjas', (req, res) => {
+  res.send({type: "POST"})
+});
+
+router.put('/ninjas/:id', (req, res) => {
+  res.send({type: "PUT"})
+});
+
+router.delete('/ninjas/:id', (req, res) => {
+  res.send({type: "DELETE"})
+});
