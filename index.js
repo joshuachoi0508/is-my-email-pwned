713f5b56438fc2524e2edbcef5f1efe8b1264b65
Josh Choi <joshuachoi0508@gmail.com>
2019-05-14 16:46:26 -0700

    Set up dummy get request

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/', (req, res) => {
+  console.log('GET request');
+  res.send({name: 'Yoshi'});
+  res.end();
+})
+
 //listen for requests
 app.listen(process.env.port || 4000, () => {
   console.log('now listening for requests!');
