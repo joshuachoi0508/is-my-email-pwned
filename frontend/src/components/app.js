67f628a0e4278490cd7be26ec7d8aaf02b79ee4b
Josh Choi <joshuachoi0508@gmail.com>
2019-05-15 15:13:46 -0700

    Create app.js

---
+++ frontend/src/components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './reset.scss'; 
+
+const App = () => (
+  <div>
+    <p>HELLO</p>
+  </div>
+);
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Root from './components/root';
 import configureStore from './store/store';
+
 ReactDOM.render(
   <Root store={configureStore({})} />, 
   document.getElementById('root')
