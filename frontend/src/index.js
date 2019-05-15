84d1a10e680f5eb367127efe8d18b242c6d6882a
Josh Choi <joshuachoi0508@gmail.com>
2019-05-15 14:08:54 -0700

    Write index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Root store={configureStore({})} />, 
+    document.getElementById('root')
+  )
+});
