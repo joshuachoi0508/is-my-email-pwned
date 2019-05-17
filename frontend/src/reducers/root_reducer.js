e6d8b168fac02f6ef66ae4332e115488d008e86f
Josh Choi <joshuachoi0508@gmail.com>
2019-05-17 13:14:25 -0700

    Adjust body min-height

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import { RECEIVE_PWNED_INFO } from '../actions/actions';
 
 export default function (prevState = {}, action) {
-  debugger;
   Object.freeze(prevState);
+  
   switch (action.type) {
     case RECEIVE_PWNED_INFO:
       return Object.assign({}, {result: action.result.data});
