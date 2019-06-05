66608d118c1f4ac1fd8c918a0f0f327e0f27fbf8
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 09:58:52 -0700

    Move then/catch out

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,9 @@ const fetch = require("node-fetch");
 router.post('/pwned', (req, res) => {
   const url = "https://haveibeenpwned.com/api/breachedaccount/";
   fetch(url + req.body.email)
-    .then(response => {
-      response.json()
-        .then(result => res.json(result))
-        .catch(error => res.json(error));
-    })
+    .then(response => (response.json()))
+    .then(result => res.json(result))
+    .catch(error => res.json(error));
 })
 
 module.exports = router;
