3ca729fafb95f0becf6880b054279d4fa19dcd51
Josh Choi <joshuachoi0508@gmail.com>
2019-06-05 11:33:39 -0700

    .

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/public'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
